7183ae640a946f19d811517863c1107bdbb5a6ea
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2021-10-31 01:02:45 -0300

    Implementado tela inicial com bootstrap 5.1

---
+++ Helpers/SEOPageMetaHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WPASolutions_V6.Helpers
+{
+    public static class SEOPageMetaHelper
+    {
+        public static IEnumerable<Tuple<string, string, string, string>> Collections
+        {
+            get
+            {
+                return new List<Tuple<string, string, string, string>>
+                {
+                    new Tuple<string, string, string, string>("Home/Index", " WPA Solutions - Soluções em Tecnologia e Publicidade - Paraíso do Tocantins -TO",
+                    "Site Institucional, E-Commerce , Apps para Android e IOS, Designer Gráfico , Comunicação Visual e Marketing Digital",
+                    "Desenvolvimento de sites Desenvolvimento de aplicativos para android  iphone Designer Gráfico  Comunicação Visual  Marketing Digital Paraíso do Tocantins")
+                };
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using WPASolutions_V6.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<HelpersServices>();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
