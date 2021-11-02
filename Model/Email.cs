99eab8271074a594c0db8bb5be1b12061791f630
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2021-11-02 16:26:22 -0300

    Adicionado nomeclaturas ao DisplayName

--- Model/Email.cs
+++ Model/Email.cs
@@ -7,18 +7,22 @@ namespace WPASolutions_V6.Model
         [Required(ErrorMessage = "O campo {0} deve ser preenchido!")]
         [MinLength(2,ErrorMessage ="O Campo {0} deve conter no mínino {1} caracteres!")]
         [MaxLength(20,ErrorMessage ="O campo {0} deve conter no máximo {1} caracteres!")]
+        [Display(Name ="Nome")]
         public string Nome { get; private set; }
         [Required(ErrorMessage = "O campo {0} deve ser preenchido!")]
         [MinLength(4, ErrorMessage = "O Campo {0} deve conter no mínino {1} caracteres!")]
         [MaxLength(70, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres!")]
+        [Display(Name = "Sobrenome")]
         public string Sobrenome { get; private set; }
         [Required(ErrorMessage = "O campo {0} deve ser preenchido!")]
         [EmailAddress(ErrorMessage = "O campo {0} deve ser preenchido corretamente!")]
+        [Display(Name = "Email")]
         public string Emails { get; private set; }
         public IEnumerable<string> Assunto { get; private set; } = new List<string>() { "Arte gráfica", "Publicidade e Markenting", "Sites", "E-Commerce", "Aplicativos para Smartphones" };
         [Required(ErrorMessage = "O campo {0} deve ser preenchido!")]
         [MinLength(5, ErrorMessage = "O Campo {0} deve conter no mínino {1} caracteres!")]
         [MaxLength(500, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres!")]
+        [Display(Name = "Mensagem")]
         public string Mensagem { get; private set; }
         public Email(string nome, string sobrenome, string emails, IEnumerable<string> assunto, string mensagem)
         {
