0a2b0083cc74da0d52c8e08b775b4c4c32bf618c
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2021-11-02 22:14:39 -0300

    Corrigido tela de contatos e excluimos o serviço de SMTP justamente com model

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<HelpersServices>();
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
