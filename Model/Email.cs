060564b91c74bc891fb4372168f2d78cc81d2667
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2021-11-02 21:48:08 -0300

    Criação de tela de mensagem em contatos

--- Model/Email.cs
+++ Model/Email.cs
@@ -5,32 +5,25 @@ namespace WPASolutions_V6.Model
     public class Email
     {
         [Required(ErrorMessage = "O campo {0} deve ser preenchido!")]
-        [MinLength(2,ErrorMessage ="O Campo {0} deve conter no mínino {1} caracteres!")]
-        [MaxLength(20,ErrorMessage ="O campo {0} deve conter no máximo {1} caracteres!")]
-        [Display(Name ="Nome")]
-        public string Nome { get; private set; }
+        [MinLength(2, ErrorMessage = "O Campo {0} deve conter no mínino {1} caracteres!")]
+        [MaxLength(20, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres!")]
+        [Display(Name = "Nome")]
+        public string Nome { get;  set; } 
         [Required(ErrorMessage = "O campo {0} deve ser preenchido!")]
         [MinLength(4, ErrorMessage = "O Campo {0} deve conter no mínino {1} caracteres!")]
         [MaxLength(70, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres!")]
         [Display(Name = "Sobrenome")]
-        public string Sobrenome { get; private set; }
+        public string Sobrenome { get;  set; }
         [Required(ErrorMessage = "O campo {0} deve ser preenchido!")]
         [EmailAddress(ErrorMessage = "O campo {0} deve ser preenchido corretamente!")]
         [Display(Name = "Email")]
-        public string Emails { get; private set; }
-        public IEnumerable<string> Assunto { get; private set; } = new List<string>() { "Arte gráfica", "Publicidade e Markenting", "Sites", "E-Commerce", "Aplicativos para Smartphones" };
+        public string Emails { get;  set; }
+        public IEnumerable<string> Assunto { get;  set; } = new List<string>() { "Arte gráfica", "Publicidade e Markenting", "Sites", "E-Commerce", "Aplicativos para Smartphones" };
         [Required(ErrorMessage = "O campo {0} deve ser preenchido!")]
         [MinLength(5, ErrorMessage = "O Campo {0} deve conter no mínino {1} caracteres!")]
         [MaxLength(500, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres!")]
         [Display(Name = "Mensagem")]
-        public string Mensagem { get; private set; }
-        public Email(string nome, string sobrenome, string emails, IEnumerable<string> assunto, string mensagem)
-        {
-            Nome = nome;
-            Sobrenome = sobrenome;
-            Emails = emails;
-            Assunto = assunto;
-            Mensagem = mensagem;
-        }
+        public string Mensagem { get;  set; }
+      
     }
 }
